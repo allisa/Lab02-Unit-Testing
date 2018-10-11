1f2f82eac40c492dcb65bfdc958975534ea8bd14
Allisa LeBeuf <al5906@gmail.com>
2018-10-10 22:23:23 -0700

    started working on lab

---
+++ AtmTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Unit_Testing;
+
+namespace AtmTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DisplayBalance()
+        {
+
+        }
+    }
+}
+
+//Fact or Theory
+//Fact always hase to be true, no parameters
+//Theory is conditionally true, has to have parameters
--- Unit_Testing/Program.cs
+++ Unit_Testing/Program.cs
@@ -2,11 +2,44 @@
 
 namespace Unit_Testing
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static decimal balance = 500;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("Welcome to the ATM.");
+
+
+            //setting up menu for atm selections and collecting user input
+            //based on selection, method for that action invoked
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1: Account Balance");
+                Console.WriteLine("2: Withdraw");
+                Console.WriteLine("3: Deposit");
+                Console.WriteLine("4: Exit");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                switch(input)
+                {
+                    case 1:
+                        AccountBalance();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Your balance is: {AccountBalance}.");
+                        //Withdraw();
+                        break;
+
+
+
+                }
+            }
+
         }
     }
 }
