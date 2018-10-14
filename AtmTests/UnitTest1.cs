020fbc23085d32fab32be33b388b4df2dec1e462
Allisa LeBeuf <al5906@gmail.com>
2018-10-13 23:45:38 -0700

    Atm code working and tests passing

--- AtmTests/UnitTest1.cs
+++ AtmTests/UnitTest1.cs
@@ -7,9 +7,31 @@ namespace AtmTests
     public class UnitTest1
     {
         [Fact]
-        public void DisplayBalance()
+        public void CanDeposit()
         {
+            Program.balance = 50;
+            Assert.Equal(100, Program.Deposit(50));
+        }
 
+        [Fact]
+        public void CanWithdraw()
+        {
+            Program.balance = 500;
+            Assert.Equal(200, Program.Withdraw(300));
+        }
+
+        [Fact]
+        public void CannotGoNegative()
+        {
+            Program.balance = 100;
+            Assert.Equal(100, Program.Withdraw(150));
+        }
+
+        [Fact]
+        public void CannotDepositNegative()
+        {
+            Program.balance = 100;
+            Assert.Equal(100, Program.Deposit(-50));
         }
     }
 }
--- Unit_Testing/Program.cs
+++ Unit_Testing/Program.cs
@@ -4,42 +4,119 @@ namespace Unit_Testing
 {
     public class Program
     {
-        public static decimal balance = 500;
+        public static decimal balance = 500m;
 
         public static void Main(string[] args)
         {
-            
-
             Console.WriteLine("Welcome to the ATM.");
+            AtmMenu();
 
 
-            //setting up menu for atm selections and collecting user input
-            //based on selection, method for that action invoked
+        }
+
+        /// <summary>
+        /// Method to display ATM menu and get user input for ATM options
+        /// </summary>
+        public static void AtmMenu()
+        {
             bool exit = false;
             while (!exit)
             {
                 Console.WriteLine("Please select an option:");
-                Console.WriteLine("1: Account Balance");
-                Console.WriteLine("2: Withdraw");
-                Console.WriteLine("3: Deposit");
-                Console.WriteLine("4: Exit");
+                Console.WriteLine("|1: Account Balance|; |2: Withdraw|; |3: Deposit|; |4: Exit|");
                 int input = Convert.ToInt32(Console.ReadLine());
 
-                switch(input)
+                switch (input)
                 {
                     case 1:
                         AccountBalance();
                         break;
                     case 2:
-                        Console.WriteLine($"Your balance is: {AccountBalance}.");
-                        //Withdraw();
+                        Console.WriteLine($"Your balance is: {AccountBalance()}. How much do you want to withdraw?");
+                        int amtToWithdraw = Convert.ToInt32(Console.ReadLine());
+                        Withdraw(amtToWithdraw);
                         break;
-
-
-
+                    case 3:
+                        Console.WriteLine("How much would you like to deposit?");
+                        int amtToDeposit = Convert.ToInt32(Console.ReadLine());
+                        Deposit(amtToDeposit);
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for banking, come again.");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Not an option, please try again.");
+                        break;
                 }
             }
+        }
 
+        /// <summary>
+        /// Method to show account balance or throw error
+        /// </summary>
+        /// <returns></returns>
+        public static decimal AccountBalance()
+        {
+            try
+            {
+                Console.WriteLine($"Your current balance is: ${balance}");
+                return balance;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("An error has occured.");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Method to withdraw money from balance
+        /// </summary>
+        /// <param name="dollarAmt"></param>
+        /// <returns></returns>
+        public static decimal Withdraw(decimal dollarAmt)
+        {
+            try
+            {
+                if (dollarAmt > balance)
+                {
+                    Console.WriteLine("Not enough funds");
+                    return balance;
+                }
+                else
+                {
+                    return balance -= dollarAmt;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Method to deposit money to balance
+        /// </summary>
+        /// <param name="dollarAmt"></param>
+        /// <returns></returns>
+        public static decimal Deposit (decimal dollarAmt)
+        {
+            try
+            {
+                if (dollarAmt < 0)
+                {
+                    return balance;
+                }
+                else
+                {
+                    return balance += dollarAmt;
+                }
+            }
+            catch(Exception)
+            {
+                throw;   
+            }
         }
     }
 }
